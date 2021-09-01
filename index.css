4f7f6ee22915b2ba6b31b2ab50350e3998ccbdbc
chris <tyoukyouchris@gmail.com>
2021-09-01 17:40:49 +0800

    first commit

---
+++ index.css
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="ja"><!-- InstanceBegin template="/Templates/form.dwt" codeOutsideHTMLIsLocked="false" -->
+<head prefix="og:http://ogp.me/ns#">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no, address=no, email=no">
+<meta name="Keywords" content="エッチボイス,官能,ボイス,エロ,女性向け,音声,バイノーラル,乙女ボイス,youtube,無料">
+<meta name="Description" content="女性向けのエッチボイスを無料で公開中！女子が好きなえっちな官能ボイス、シチュエーションCDや乙女ボイス、ボーイズラブ、youtubeで公開されている作品など男性のエロい声がたくさん聴けます♪バイノーラル加工、ダミーヘッドマイク使用の作品も多数！あなたの好みのセクシーボイスを見つけてエロボイスを堪能してみませんか？">
+<!-- InstanceBeginEditable name="ttl" -->
+<title>新人報到  | テスト 公式サイト</title>
+<!-- InstanceEndEditable -->
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta http-equiv="Pragma content=no-cache">
+<meta http-equiv="Expires" content="0">
+<meta name="author" content="さくらの恋猫">
+<link rel="canonical" href="http://sakurakoineko.com/">
+<link rel="shortcut icon" href="../common/images/favicon.ico">
+<link rel="apple-touch-icon" href="../common/images/apple-touch-icon.png">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+<link href="../css/swiper.min.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
+	
+<link href="../css/form.css" rel="stylesheet">
+<link href="../css/login.css" rel="stylesheet">
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+
+<!-- ▼wrapper▼ -->
+<div id="wrapper"> 
+  
+  <!-- ▼header▼ -->
+  <header id="header" class="style2">
+    <h1 class="top_txt">サンプルサンプルサンプルサンプルサンプルサンプルサンプル</h1>
+    <div class="box">
+      <p class="logo"><img src="../common/images/logo.png" alt="さくらの恋猫" width="200" height="28"></p>
+    </div>
+  </header>
+  <!-- ▲header▲ --> 
+  
+  <!-- ▼main▼ -->
+	<main id="main" class="no_bg"> 
+<!-- InstanceBeginEditable name="form" -->
+    <!-- ▼Contents_area▼ -->
+    <div id="Contents_area">
+     <h1>～我是一个天大的标题～</h1>
+      <div class="inner4 sp_mini">
+        <h2 class="ttl_style06 mb20 color2">新人報到</h2>
+        <div class="tab_box color2 mb200 sp_mb140">
+          <ul class="tab_btn">
+            <li class="on"><span>携帯番号で登録</span></li>
+            <li><a href="#">メールアドレスで登録</a></li>
+          </ul>
+          <div class="tab_box">
+            <div class="box">
+			<h3 class="mb10">携帯番号を入力してください。</h3>
+              <div data-v-0134d02e="" class="mobile-item mb20">
+                <input data-v-0134d02e="" type="text" id="inputMobile" placeholder="09000000000" required="required" autofocus="autofocus" class="form-control">
+                <div data-v-0134d02e="" class="area-code">+81</div>
+				 <div class="select_wrap">
+                <select data-v-0134d02e="" id="region" class="form-control">
+                  <option data-v-0134d02e="" selected="selected" value="86">CN</option>
+                  <option data-v-0134d02e="" value="852">HK</option>
+                  <option data-v-0134d02e="" value="853">MO</option>
+                  <option data-v-0134d02e="" value="886">TW</option>
+                  <option data-v-0134d02e="" value="60">MY</option>
+                  <option data-v-0134d02e="" value="62">ID</option>
+                  <option data-v-0134d02e="" value="63">PH</option>
+                  <option data-v-0134d02e="" value="65">SG</option>
+                  <option data-v-0134d02e="" value="66">TH</option>
+                  <option data-v-0134d02e="" value="81">JP</option>
+                  <option data-v-0134d02e="" value="82">KR</option>
+                  <option data-v-0134d02e="" value="7">TJ</option>
+                  <option data-v-0134d02e="" value="7">KZ</option>
+                  <option data-v-0134d02e="" value="84">VN</option>
+                  <option data-v-0134d02e="" value="90">TR</option>
+                  <option data-v-0134d02e="" value="91">IN</option>
+                  <option data-v-0134d02e="" value="92">PK</option>
+                  <option data-v-0134d02e="" value="93">AF</option>
+                  <option data-v-0134d02e="" value="94">LK</option>
+                  <option data-v-0134d02e="" value="95">MM</option>
+                  <option data-v-0134d02e="" value="98">IR</option>
+                  <option data-v-0134d02e="" value="374">AM</option>
+                  <option data-v-0134d02e="" value="673">BN</option>
+                  <option data-v-0134d02e="" value="850">KP</option>
+                  <option data-v-0134d02e="" value="855">KH</option>
+                  <option data-v-0134d02e="" value="856">LA</option>
+                  <option data-v-0134d02e="" value="880">BD</option>
+                  <option data-v-0134d02e="" value="960">MV</option>
+                  <option data-v-0134d02e="" value="961">LB</option>
+                  <option data-v-0134d02e="" value="962">JO</option>
+                  <option data-v-0134d02e="" value="963">SY</option>
+                  <option data-v-0134d02e="" value="964">IQ</option>
+                  <option data-v-0134d02e="" value="965">KW</option>
+                  <option data-v-0134d02e="" value="966">SA</option>
+                  <option data-v-0134d02e="" value="967">YE</option>
+                  <option data-v-0134d02e="" value="968">OM</option>
+                  <option data-v-0134d02e="" value="970">BL</option>
+                  <option data-v-0134d02e="" value="971">AE</option>
+                  <option data-v-0134d02e="" value="972">IL</option>
+                  <option data-v-0134d02e="" value="973">BH</option>
+                  <option data-v-0134d02e="" value="974">QA</option>
+                  <option data-v-0134d02e="" value="975">BT</option>
+                  <option data-v-0134d02e="" value="976">MN</option>
+                  <option data-v-0134d02e="" value="977">NP</option>
+                  <option data-v-0134d02e="" value="993">TM</option>
+                  <option data-v-0134d02e="" value="994">AZ</option>
+                  <option data-v-0134d02e="" value="998">UZ</option>
+                  <option data-v-0134d02e="" value="44">GB</option>
+                  <option data-v-0134d02e="" value="49">DE</option>
+                  <option data-v-0134d02e="" value="39">IT</option>
+                  <option data-v-0134d02e="" value="33">FR</option>
+                  <option data-v-0134d02e="" value="7">RU</option>
+                  <option data-v-0134d02e="" value="30">GR</option>
+                  <option data-v-0134d02e="" value="31">NL</option>
+                  <option data-v-0134d02e="" value="32">BE</option>
+                  <option data-v-0134d02e="" value="34">ES</option>
+                  <option data-v-0134d02e="" value="36">HU</option>
+                  <option data-v-0134d02e="" value="40">RO</option>
+                  <option data-v-0134d02e="" value="41">CH</option>
+                  <option data-v-0134d02e="" value="43">AT</option>
+                  <option data-v-0134d02e="" value="45">DK</option>
+                  <option data-v-0134d02e="" value="46">SE</option>
+                  <option data-v-0134d02e="" value="47">NO</option>
+                  <option data-v-0134d02e="" value="48">PL</option>
+                  <option data-v-0134d02e="" value="223">SM</option>
+                  <option data-v-0134d02e="" value="336">HU</option>
+                  <option data-v-0134d02e="" value="338">YU</option>
+                  <option data-v-0134d02e="" value="350">GI</option>
+                  <option data-v-0134d02e="" value="351">PT</option>
+                  <option data-v-0134d02e="" value="352">LU</option>
+                  <option data-v-0134d02e="" value="353">IE</option>
+                  <option data-v-0134d02e="" value="354">IS</option>
+                  <option data-v-0134d02e="" value="355">AL</option>
+                  <option data-v-0134d02e="" value="356">MT</option>
+                  <option data-v-0134d02e="" value="357">CY</option>
+                  <option data-v-0134d02e="" value="358">FI</option>
+                  <option data-v-0134d02e="" value="359">BG</option>
+                  <option data-v-0134d02e="" value="370">LT</option>
+                  <option data-v-0134d02e="" value="371">LV</option>
+                  <option data-v-0134d02e="" value="372">EE</option>
+                  <option data-v-0134d02e="" value="373">MD</option>
+                  <option data-v-0134d02e="" value="376">AD</option>
+                  <option data-v-0134d02e="" value="380">UA</option>
+                  <option data-v-0134d02e="" value="381">YU</option>
+                  <option data-v-0134d02e="" value="385">HR</option>
+                  <option data-v-0134d02e="" value="386">SI</option>
+                  <option data-v-0134d02e="" value="396">VA</option>
+                  <option data-v-0134d02e="" value="420">CZ</option>
+                  <option data-v-0134d02e="" value="421">SK</option>
+                  <option data-v-0134d02e="" value="423">LI</option>
+                  <option data-v-0134d02e="" value="51">PE</option>
+                  <option data-v-0134d02e="" value="52">MX</option>
+                  <option data-v-0134d02e="" value="53">CU</option>
+                  <option data-v-0134d02e="" value="54">AR</option>
+                  <option data-v-0134d02e="" value="55">BR</option>
+                  <option data-v-0134d02e="" value="56">CL</option>
+                  <option data-v-0134d02e="" value="57">CO</option>
+                  <option data-v-0134d02e="" value="58">VE</option>
+                  <option data-v-0134d02e="" value="501">BZ</option>
+                  <option data-v-0134d02e="" value="502">GT</option>
+                  <option data-v-0134d02e="" value="503">SV</option>
+                  <option data-v-0134d02e="" value="504">HN</option>
+                  <option data-v-0134d02e="" value="505">NI</option>
+                  <option data-v-0134d02e="" value="506">CR</option>
+                  <option data-v-0134d02e="" value="507">PA</option>
+                  <option data-v-0134d02e="" value="509">HT</option>
+                  <option data-v-0134d02e="" value="590">GP</option>
+                  <option data-v-0134d02e="" value="591">BO</option>
+                  <option data-v-0134d02e="" value="592">GY</option>
+                  <option data-v-0134d02e="" value="593">EC</option>
+                  <option data-v-0134d02e="" value="594">GF</option>
+                  <option data-v-0134d02e="" value="595">PY</option>
+                  <option data-v-0134d02e="" value="596">MQ</option>
+                  <option data-v-0134d02e="" value="597">SR</option>
+                  <option data-v-0134d02e="" value="598">UY</option>
+                  <option data-v-0134d02e="" value="20">EG</option>
+                  <option data-v-0134d02e="" value="27">ZA</option>
+                  <option data-v-0134d02e="" value="212">MA</option>
+                  <option data-v-0134d02e="" value="213">DZ</option>
+                  <option data-v-0134d02e="" value="216">TN</option>
+                  <option data-v-0134d02e="" value="218">LY</option>
+                  <option data-v-0134d02e="" value="220">GM</option>
+                  <option data-v-0134d02e="" value="221">SN</option>
+                  <option data-v-0134d02e="" value="222">MR </option>
+                  <option data-v-0134d02e="" value="223">ML</option>
+                  <option data-v-0134d02e="" value="224">GN</option>
+                  <option data-v-0134d02e="" value="227">NE</option>
+                  <option data-v-0134d02e="" value="228">TG</option>
+                  <option data-v-0134d02e="" value="229">BJ</option>
+                  <option data-v-0134d02e="" value="230">MU</option>
+                  <option data-v-0134d02e="" value="231">LR</option>
+                  <option data-v-0134d02e="" value="232">SL</option>
+                  <option data-v-0134d02e="" value="233">GH</option>
+                  <option data-v-0134d02e="" value="234">NG</option>
+                  <option data-v-0134d02e="" value="235">TD</option>
+                  <option data-v-0134d02e="" value="237">CM</option>
+                  <option data-v-0134d02e="" value="241">GA</option>
+                  <option data-v-0134d02e="" value="242">CG</option>
+                  <option data-v-0134d02e="" value="243">ZR</option>
+                  <option data-v-0134d02e="" value="244">AO</option>
+                  <option data-v-0134d02e="" value="248">SC</option>
+                  <option data-v-0134d02e="" value="249">SD</option>
+                  <option data-v-0134d02e="" value="251">ET</option>
+                  <option data-v-0134d02e="" value="252">SO</option>
+                  <option data-v-0134d02e="" value="253">DJ</option>
+                  <option data-v-0134d02e="" value="254">KE</option>
+                  <option data-v-0134d02e="" value="255">TZ</option>
+                  <option data-v-0134d02e="" value="256">UG</option>
+                  <option data-v-0134d02e="" value="257">BI</option>
+                  <option data-v-0134d02e="" value="258">MZ</option>
+                  <option data-v-0134d02e="" value="260">ZM</option>
+                  <option data-v-0134d02e="" value="261">MG</option>
+                  <option data-v-0134d02e="" value="263">ZW</option>
+                  <option data-v-0134d02e="" value="264">NA</option>
+                  <option data-v-0134d02e="" value="265">MW</option>
+                  <option data-v-0134d02e="" value="266">LS</option>
+                  <option data-v-0134d02e="" value="267">BW</option>
+                  <option data-v-0134d02e="" value="268">SZ</option>
+                  <option data-v-0134d02e="" value="377">MC</option>
+                  <option data-v-0134d02e="" value="61">AU</option>
+                  <option data-v-0134d02e="" value="64">NZ</option>
+                  <option data-v-0134d02e="" value="671">GU</option>
+                  <option data-v-0134d02e="" value="674">NR</option>
+                  <option data-v-0134d02e="" value="676">TO</option>
+                  <option data-v-0134d02e="" value="677">SB</option>
+                  <option data-v-0134d02e="" value="679">FJ</option>
+                  <option data-v-0134d02e="" value="1">US</option>
+                  <option data-v-0134d02e="" value="1">CA</option>
+                  <option data-v-0134d02e="" value="1809">PR</option>
+                  <option data-v-0134d02e="" value="1809">BS</option>
+                  <option data-v-0134d02e="" value="1809">AI</option>
+                  <option data-v-0134d02e="" value="1809">LC</option>
+                  <option data-v-0134d02e="" value="1809">BB</option>
+                  <option data-v-0134d02e="" value="1876">JM</option>
+                  <option data-v-0134d02e="" value="64672">AQ</option>
+                </select>
+              </div>
+				</div>
+
+			</div>
+          </div>
+        </div>
+		 </div>
+		<div class="inner4">
+		 <div class="btn_wrap">
+			 <button type="button" class="btn_style bd_color_green txt_big_style mb10">確認コードを受信する<i class="ml20"><img src="../common/images/icon_phone@2x.png" alt="新規登録" width="13" height="20"></i></button>
+			 <p class="txt_small_style txt_att_style mb40">携帯にショートメッセージで送られてくる<span class="bold">６桁の確認コード</span>を受信して下さい。</p>
+
+			 <div class="flex_style jc_sb">
+				<p class="btn_nowrap_style color1 style_left"><a href="#"><span>トップ</span></a></p>
+				<p class="btn_nowrap_style color1"><a href="#"><span>ログイン</span></a></p>
+			</div>
+		</div>
+			  
+		  
+      </div>
+    </div>
+    <!-- ▲Contents_area▲ --> 
+  <!-- InstanceEndEditable -->
+	</main>
+  <!-- ▲main▲ --> 
+  
+  <!-- ▼footer▼ -->
+  <footer id="footer" class="style2">
+    <div class="btm">
+      <div class="inner2"> <small>COPYRIGHT © NATURAL PLANTS PUBLISHING ALL RIGHTS RESERVED.</small> </div>
+    </div>
+  </footer>
+  <!-- ▲footer▲ --> 
+  
+</div>
+<!-- ▲wrapper▲ --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+<script> (window.jQuery || document .write('<script src="js/jquery-3.5.1.min.js"><\/script>')); </script> 
+<script src="../js/swiper.min.js"></script> 
+<script src="../js/common.js"></script>
+<script src="../js/ofi.min.js"></script>
+<script>
+  objectFitImages('img.object-fit-img');
+</script>
+</body>
+<!-- InstanceEnd --></html>
